aaed515583cd27dfe186f1e608f26e104c8eef10
p-nasimonan <pnasimonan@gmail.com>
2026-03-06 03:56:49 +0900

    リンク

---
+++ src/consts/links.ts
@@ -0,0 +1,33 @@
+export type SocialLink = {
+	label: string;
+	url: string;
+	color: string;
+	rotate: string;
+};
+
+export const socialLinks: SocialLink[] = [
+	{
+		label: "Twitter",
+		url: "https://x.com/youkan0124",
+		color: "#1da1f2",
+		rotate: "-10deg",
+	},
+	{
+		label: "GitHub",
+		url: "https://github.com/p-nasimonan",
+		color: "#333",
+		rotate: "5deg",
+	},
+	{
+		label: "Misskey",
+		url: "https://mi.youkan.uk/@youkan",
+		color: "#9146ff",
+		rotate: "5deg",
+	},
+	{
+		label: "Bluesky",
+		url: "https://bsky.app/profile/youkan.uk",
+		color: "#0085ff",
+		rotate: "-5deg",
+	},
+];
